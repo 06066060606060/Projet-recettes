3323acf772717d4c44a7038e6972c0ca932449e3
06066060606060 <xmicky+git@hotmail.fr>
2022-04-28 20:39:25 +0400

    jeudi ajout grid 9

---
+++ js/Myfunctions.js
@@ -0,0 +1,14 @@
+
+  function on() {
+    console.log("okiii");
+        document.getElementById("overlay").style.display = "block";
+    document.getElementById("login").style.visibility = "visible";
+    document.getElementById("login").style.zIndex = "2";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("login").style.zIndex = "-1";
+  }
+  
